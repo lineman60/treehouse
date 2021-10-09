1f94cbed08117b428cb8fa620028c12406743bb7
lineman60 <snyder60>
2021-10-09 14:35:02 -0500

    with lists

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,27 @@ fn what_is_your_name() -> String{
         .read_line(&mut your_name)
         .expect("Failed to read line");
     your_name
+        .trim()
+        .to_lowercase()
+
 }
 
 fn main() {
     println!("Hello, what's your name?");
     let name = what_is_your_name();
-    println!("Hello, {}", name);
+    let mut allow_them_in = false;
+    let vistor_list = ["foo","bar","biz"];
+    for visitor in &vistor_list{
+        if visitor == &name{
+            allow_them_in = true;
+        }
+    }
+    if allow_them_in == true{
+        println!("Hello, {:?} welcome to the treehouse", name);
+    } else {
+        println!("Sorry, you're not welcome");
+    }
+
+
+
 }
