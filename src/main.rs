850928f9501eb96409310e276ef682c4c893e191
lineman60 <snyder60>
2021-10-13 23:08:09 -0500

    mid loop add

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,45 @@
 use std::io::stdin;
 
-struct Visitor{
+#[derive(Debug)]
+struct Visitor {
     name: String,
     greeting: String,
 }
 
-impl Visitor{
-    fn new(name: &str, greeting: &str) -> Self{
-        Self{
+impl Visitor {
+    fn new(name: &str, greeting: &str) -> Self {
+        Self {
             name: name.to_lowercase(),
             greeting: greeting.to_string(),
         }
     }
-    fn greet_visitor(&self){
+    fn greet_visitor(&self) {
         println!("{}", self.greeting)
     }
 }
 
-fn what_is_your_name() -> String{
+fn what_is_your_name() -> String {
     let mut your_name = String::new();
     stdin()
         .read_line(&mut your_name)
         .expect("Failed to read line");
-    your_name
-        .trim()
-        .to_lowercase()
-
+    your_name.trim().to_lowercase()
 }
 
 fn main() {
-    println!("Hello, what's your name?");
-    let name = what_is_your_name();
-    let visitor_list = [
-        Visitor::new("foo", "The beer is in the fridge"),
-        Visitor::new("bar", "Remember to tip the waitstaff"),
-        Visitor::new("biz", "who invited you?"),
-                     ];
-    let known_visitor = visitor_list
-        .iter()
-        .find(|visitor|visitor.name == name);
-    match known_visitor {
-        Some(visitor) => visitor.greet_visitor(),
-        None => println!("I don't know you, Get lost")
+    loop {
+        println!("Hello, what's your name? (Leave empty to exit)");
+        let name = what_is_your_name();
+        let mut visitor_list = vec![
+            Visitor::new("foo", "The beer is in the fridge"),
+            Visitor::new("bar", "Remember to tip the waitstaff"),
+            Visitor::new("biz", "who invited you?"),
+        ];
+        let known_visitor = visitor_list.iter().find(|visitor| visitor.name == name);
+        match known_visitor {
+            Some(visitor) => visitor.greet_visitor(),
+            None => println!("I don't know you, Get lost"),
+        }
+    break;
     }
 }
